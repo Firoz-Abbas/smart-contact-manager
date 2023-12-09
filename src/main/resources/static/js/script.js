64750f9250e8b241ec7d6dd64bbf8c61cad893f4
Firoz Abbas <firozabbas.edunext@gmail.com>
2023-12-09 17:32:23 +0530

    By Firoz Abbas

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,15 @@
 console.log("this is script file")
+
+const toggleSidebar = () => {
+    if($(".sidebar").is(":visible")){
+    // do close 
+        $(".sidebarr").css("display","block");
+        $(".sidebar").css("display","none");
+        $(".content").css("margin-left","5%");
+    }else{
+    // do show
+        $(".sidebar").css("display","block");
+        $(".sidebarr").css("display","none");
+        $(".content").css("margin-left","20%");
+    }
+}
