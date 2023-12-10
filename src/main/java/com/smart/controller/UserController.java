fed36910c80f5a4321c8b02ff46befc60b2dd1d4
Firoz Abbas <firozabbas.edunext@gmail.com>
2023-12-10 23:53:45 +0530

    Firoz

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.smart.controller;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,14 +19,28 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal){
+//    method for adding conmmond data for responce
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal){
         String userName=principal.getName();
         System.out.println("user name >"+userName);
         User user=userRepository.getUserByUserName(userName);
         System.out.println("user  >"+user);
-
         model.addAttribute("user",user);
+
+    }
+//  Dashboard home
+    @RequestMapping("/index")
+    public String dashboard(Model model, Principal principal){
+        model.addAttribute("title", "user dashboard");
         return "normal/user_dashboard";
+    }
+
+//   open Add form Handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model){
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
     }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,13 +3,13 @@ console.log("this is script file")
 const toggleSidebar = () => {
     if($(".sidebar").is(":visible")){
     // do close 
-        $(".sidebarr").css("display","block");
+    //     $(".sidebarr").css("display","block");
         $(".sidebar").css("display","none");
-        $(".content").css("margin-left","5%");
+        $(".content").css("margin-left","45px");
     }else{
     // do show
         $(".sidebar").css("display","block");
-        $(".sidebarr").css("display","none");
-        $(".content").css("margin-left","20%");
+        // $(".sidebarr").css("display","none");
+        $(".content").css("margin-left","208px");
     }
 }
