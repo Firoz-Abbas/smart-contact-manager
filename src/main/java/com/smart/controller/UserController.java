67042f05ca13f769724ae6a8b2976a76a05c17cf
Firoz Abbas <firozabbas.edunext@gmail.com>
2023-12-11 18:13:21 +0530

    Firoz Abbas

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -41,6 +42,11 @@ public class UserController {
     public String openAddContactForm(Model model){
         model.addAttribute("title", "Add Contact");
         model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
+    }
+
+    @PostMapping("/process-contact")
+    public String proccessContact(@ModelAttribute Contact contact){
         return "normal/add_contact_form";
     }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,7 +5,7 @@ const toggleSidebar = () => {
     // do close 
     //     $(".sidebarr").css("display","block");
         $(".sidebar").css("display","none");
-        $(".content").css("margin-left","45px");
+        $(".content").css("margin-left","15px");
     }else{
     // do show
         $(".sidebar").css("display","block");
