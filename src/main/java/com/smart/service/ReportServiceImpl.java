8a5dee0bd08df6a6457ad4352f43aa4dcf622f4c
Firoz Abbas <firozabbas.edunext@gmail.com>
2024-01-02 18:04:17 +0530

    Firoz Abbas

--- src/main/java/com/smart/service/ReportServiceImpl.java
+++ src/main/java/com/smart/service/ReportServiceImpl.java
@@ -4,10 +4,7 @@ import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
-import org.apache.poi.hssf.usermodel.HSSFFont;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
@@ -21,9 +18,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+
+import org.apache.poi.hssf.util.*;
+import org.apache.poi.util.IOUtils;
+
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
+import java.net.URL;
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
@@ -85,9 +88,56 @@ public class ReportServiceImpl implements ReportService {
         font.setFontHeightInPoints((short) 9);
         bodyStyle.setFont(font);
 
+        //
+        Image image2 = null;
+        String percentcount = "|0|10|20|30|40|50|";
+        String gradegraph = "|A1|A2|B1|B2|C1|C2|D|E1|E2|";
+        String chartimagepath="";
+        try {
+            chartimagepath = "https://chart.googleapis.com/chart?cht=bvg&chxl=0:" + gradegraph + "|1:" + percentcount +"&chd=t:"+"20,10,15,17,45,5,8,48,9" +"&chs=900x250&chtt=&chco=151515,8c8c8c,A9A9A9&chxt=x,y&chbh=26,10,26&chm=N,000000,0,-1,10|N,000000,1,-1,10|N,000000,2,-1,10&chdl=Highest Marks|Average Marks|Marks Obtained&chxs=0,000000,9.5,0,l,000000|1,000000,9.5,0,lt,676767&chds=0,50";
+            chartimagepath = googleChartURL(chartimagepath);
+
+//            image2 = Image.getInstance(chartimagepath);
+        } catch (Exception e) {
+            System.err.println("error[" + e.getMessage());
+        }
+
+        //
+
+        /*String imagee = "E:\\11th\\IMG_2431.JPG";
+        InputStream my_banner_image = new FileInputStream(imagee);
+        byte[] bytes = IOUtils.toByteArray(my_banner_image);
+        int my_picture_id = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_JPEG);
+        my_banner_image.close();
+        HSSFPatriarch drawing = sheet.createDrawingPatriarch();
+        ClientAnchor my_anchor = new HSSFClientAnchor();
+        my_anchor.setCol1(2);
+        my_anchor.setRow1(1);
+        HSSFPicture  my_picture = drawing.createPicture(my_anchor, my_picture_id);
+        my_picture.resize(0.02);*/
+
+//         Replace with the actual URL of the image
+//        String imageUrl = "https://resources.edunexttechnologies.com/html-team/common-images/cbse-logo-blue.png";
+
+        // Fetch the image from the URL
+        URL url = new URL(chartimagepath);
+
+//        InputStream my_banner_image = new FileInputStream(imagee);
+            byte[] bytes = IOUtils.toByteArray(url.openStream());
+            int my_picture_id = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
+//                fis.close();
+
+            HSSFPatriarch drawing = sheet.createDrawingPatriarch();
+            ClientAnchor my_anchor = new HSSFClientAnchor();
+            my_anchor.setCol1(1);
+            my_anchor.setRow1(20);
+            HSSFPicture my_picture = drawing.createPicture(my_anchor, my_picture_id);
+            my_picture.resize(0.4);
+
+
         Row header_name_rowno = sheet.createRow(1);
         int columnSeq = 0;
-        int rowno=1;
+        int rowno=5;
 
         createNewHeaderCell(header_name_rowno, columnSeq++, "ID", headerStyle);
         createNewHeaderCell(header_name_rowno, columnSeq++, "Name", headerStyle);
@@ -158,7 +208,14 @@ public class ReportServiceImpl implements ReportService {
         Cell cell = row.createCell(cellno);
         cell.setCellValue(cellValue);
         cell.setCellStyle(headerStyle);
-        row.setRowStyle(headerStyle);
+//        row.setRowStyle(headerStyle);
+    }
+
+    public static String googleChartURL(String chart) {
+        chart = chart.replaceAll(" ", "");
+        chart = chart.replaceAll("\\|", "%7C");
+        chart = chart.replaceAll(" ", "");
+        return chart;
     }
 
 
